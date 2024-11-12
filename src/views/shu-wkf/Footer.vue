5f2dab862b161ecd279dc97b230dbc03412b512e
vow717 <147890905+vow717@users.noreply.github.com>
2024-11-12 18:24:34 +0800

    changeDetail

--- src/views/shu-wkf/Footer.vue
+++ src/views/shu-wkf/Footer.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <footer class="footer">
-    <p>© 2024 书法作品鉴赏与评论网站。所有 rights reserved.</p>
-    <p>2024 wkf</p>
+    <p>© 2024 书法作品鉴赏与评论网站---解释权归wkf所有</p>
   </footer>
 </template>
 
@@ -12,13 +11,5 @@
   color: black;
 
   text-align: center;
-}
-.footer a {
-  color: #fff;
-  text-decoration: none;
-  margin: 0 5px;
-}
-.footer a:hover {
-  text-decoration: underline;
 }
 </style>
