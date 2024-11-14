74f49e3aea4a79d62666f06207a3f9e53bf8b78e
Hushyo <Hushsama@hotmail.com>
2024-11-14 14:01:00 +0800

    pyh

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,19 @@ import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 //pyh
 //npm install bootstrap@latest --save
 //npm i bootstrap-icons --save
 //npm install animate.css --save
 //npm install jquery --save
+//npm install p5-ui --save
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'animate.css'
 import 'jquery'
+
 
 const app = createApp(App)
 
